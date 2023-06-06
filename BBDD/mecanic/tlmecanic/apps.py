b2b96bae56e590871da0f918cb51e7ed6de26015
Kassperzz <103401359+Kassperzz@users.noreply.github.com>
2023-06-05 20:35:13 -0400

    Revert "Avance 02-06-23"
    This reverts commit 066cb10e21302d15e1ab8031438d024530369867.

--- BBDD/mecanic/tlmecanic/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TlmecanicConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tlmecanic'
--- BBDD/mecanic/tlmecanic/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def principal(request):
-    
-    return(render (request,'index.html',context))
