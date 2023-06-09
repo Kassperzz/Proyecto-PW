326e0adbf64db3ccdd46815c236d6141035a1c4b
gonxox <135761943+gonxox@users.noreply.github.com>
2023-06-09 09:42:02 -0400

    09/06/23
    arreglo base de datos (funciona)

--- mecanic/tlmecanic/urls.py
+++ mecanic/tlmecanic/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import principal
+from .views import principal,regcliente
 
 urlpatterns = [
     path('',principal,name='principal'),
+    path('',principal,name='regcliente'),
 ]
+
--- mecanic/tlmecanic/views.py
+++ mecanic/tlmecanic/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def principal(request):
     
-    return(render (request,'index.html',context))
+    return(render (request,'index.html'))
+
+def regcliente(request):
+    
+    return(render (request,'contacto_cliente.html'))
